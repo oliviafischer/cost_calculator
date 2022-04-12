8807ac2807d55a99d86b65d564168a01c72f9813
Olivia Fischer <ofischer@fp-ws-ofischer.psycho.unibas.ch>
2022-04-12 17:34:39 +0200

    Add CloudResearch cost and update bonus calcuation

--- riskpol_shiny.R
+++ riskpol_shiny.R
@@ -1,9 +1,12 @@
 library(shiny)
 library(tidyverse)
 
+
+# UI ----------------------------------------------------------------------
+
 ui <- fluidPage(
   
-  titlePanel("Calculate study cost"),
+  titlePanel("Study cost calculator"),
   
   # Choice of recruitment platform
   # radioButtons("platform", 
@@ -25,13 +28,13 @@ ui <- fluidPage(
            numericInput("n_t1",
                         "Number of participants T1:",
                         min = 0,
-                        value = 100)),
+                        value = 1000)),
     
     column(4,
            numericInput("t_t1",
                         "Length of study T1 [minutes]:",
                         min = 0,
-                        value = 5)),
+                        value = 10)),
     
     # column(4,
     #        radioButtons("masters",
@@ -41,10 +44,10 @@ ui <- fluidPage(
     
     column(4,
            numericInput("n_premium",
-                        "Number of premium qualifications (MTurk):",
+                        "Number of premium qualifications*:",
                         min = 0,
-                        value = 0,
-                        step = .5))
+                        max = 2,
+                        value = 0))
     
   ),
   
@@ -53,13 +56,13 @@ ui <- fluidPage(
            numericInput("n_t2",
                         "Number of participants T2:",
                         min = 0,
-                        value = 100)),
+                        value = 0)),
     
     column(4,
            numericInput("t_t2",
                         "Length of study T2 [minutes]:",
                         min = 0,
-                        value = 5))
+                        value = 0))
     
   ),
   
@@ -68,13 +71,13 @@ ui <- fluidPage(
            numericInput("n_t3",
                         "Number of participants T3:",
                         min = 0,
-                        value = 100)),
+                        value = 0)),
     
     column(4,
            numericInput("t_t3",
                         "Length of study T3 [minutes]:",
                         min = 0,
-                        value = 5))
+                        value = 0))
     
   ),
   
@@ -92,7 +95,8 @@ ui <- fluidPage(
            numericInput("bonus",
                         "Bonus payment [$]:",
                         min = 0,
-                        value = 1))
+                        value = 0,
+                        step = .5))
     
   ),
 
@@ -106,17 +110,21 @@ ui <- fluidPage(
   withTags({
     div("Notes:",
     ul(
-        li("Service fee MTurk (", 
+        li("MTurk service fee (", 
            a(href = "https://requester.mturk.com/pricing", "Click here", target="_blank"), 
            "for pricing details): 20%"),
         br(),
-        li("Service fee MTurk for large (>9) assignments/participants: 40%"),
+        li("MTurk service fee for large (>9) assignments/participants: 40%"),
         br(),
         li("Additional service fee for MTurk Masters Qualifications (higher quality participants: 5%)"),
         br(),
-        li("Premium qualifications (MTurk only): median additional fee of $0.40 per assignment"),
+        li("*Premium qualifications (MTurk only): median additional fee of $0.40 per assignment (range from $0.05 to $1.00); MTurk allows a maximum of 2 premium qualifications per HIT"),
         br(),
-        li("Service fee Prolific (",
+        li("CloudResearch service fee (",
+           a(href = "https://go.cloudresearch.com/en/knowledge/turkprime-fees-for-using-the-mturk-toolkit", "Click here", target="_blank"),
+           "for pricing details): 10% plus 20% MTurk service fee; no CR fee for bonuses (standard MTurk fee)"),
+        br(),
+        li("Prolific service fee (",
            a(href = "https://www.prolific.co/pricing", "Click here", target="_blank"),
            "for pricing details): 33%")
         ))
@@ -129,21 +137,52 @@ ui <- fluidPage(
 ) # closing bracket for fluidPage
 
 
+
+# SERVER ------------------------------------------------------------------
+
 # Define server logic for slider examples ----
 server <- function(input, output) {
   
   # Reactive expression to create data frame of all input values ----
   numericValues <- reactive({
-    
-      # Participant payment
-      part_pay <- sum(
-      input$n_t1*(input$t_t1/60*input$rate),
-      input$n_t2*(input$t_t2/60*input$rate),
-      input$n_t3*(input$t_t3/60*input$rate)
-    )
+  
       
+      # total N over all studies
       n_tot <- sum(input$n_t1, input$n_t2, input$n_t3)
+      
+      # total study time (in minutes)
       t_tot <- sum(input$t_t1, input$t_t2, input$t_t3)
+      
+      # Participant payment excl. bonus
+      part_pay <- sum(
+        input$n_t1*(input$t_t1/60*input$rate),
+        input$n_t2*(input$t_t2/60*input$rate),
+        input$n_t3*(input$t_t3/60*input$rate)
+      )
+      
+      # participant payment excl. bonus
+      # part_pay <- n_tot * (t_tot / 60 * input$rate)
+      
+      # bonus payment
+      bonus <- if_else(input$n_t3 != 0 & input$t_t3 != 0, 
+                       input$n_t3 * input$bonus, 
+                       if_else(input$n_t2 != 0 & input$t_t2 != 0, 
+                               input$n_t2 * input$bonus,
+                               input$n_t1 * input$bonus))
+      
+      
+      
+      # if(input$n_t3 != 0 & input$t_t3 != 0){
+      #   bonus <- input$n_t3 * input$bonus
+      # } else if(input$n_t2 != 0 & input$t_t2 != 0){
+      #   bonus <- input$n_t2 * input$bonus
+      # } else (input$n_t1 != 0 & input$t_t1 != 0){
+      #   bonus <- input$n_t1 * input$bonus
+      # }
+        
+      # total payment for participants
+      tot_pay <- part_pay + bonus
+      
       
 
       # Platform payment/service fee
@@ -163,23 +202,31 @@ server <- function(input, output) {
       #         mturk_fee <- (part_pay + input$bonus) * 100
       #         )
       # 
-      # Amazon
+      
+      
+      ## Service fees
+      # MTurk
       
       premium_fee <- .4
       
-      mturk_fee <- (part_pay + input$bonus) * .2 + input$n_premium * premium_fee *  n_tot
+      mturk_fee <- (part_pay + bonus) * .2 + input$n_premium * premium_fee *  n_tot
       
-      # Amazon + Masters qual
-      masters_fee <- (part_pay + input$bonus) * .2 + part_pay * .05 + input$n_premium * premium_fee *  n_tot
+      # MTurk + Masters qual
+      masters_fee <- (part_pay + bonus) * .2 + part_pay * .05 + input$n_premium * premium_fee *  n_tot
+      
+      # CloudResearch (MTurk + CR fee)
+      # CR does not issue fee for bonuses
+      cr_fee <- (part_pay) * .3 + bonus * .2 + input$n_premium * premium_fee *  n_tot
       
       # Prolific
-      prolific_fee <- (part_pay + input$bonus) * .33
+      prolific_fee <- (part_pay + bonus) * .33
       
 
       # Total pay
-      tot_pay_mturk <- part_pay + mturk_fee
-      tot_pay_masters <- part_pay + masters_fee
-      tot_pay_prolific <- part_pay + prolific_fee
+      tot_pay_mturk <- tot_pay + mturk_fee
+      tot_pay_masters <- tot_pay + masters_fee
+      tot_pay_cr <- tot_pay + cr_fee
+      tot_pay_prolific <- tot_pay + prolific_fee
     
 
     # Prolific cost breakdown
@@ -187,15 +234,18 @@ server <- function(input, output) {
       Component = c("Participant payment",
                     "Service fee",
                     "Total"),
-      Cost.MTurk = as.character(c(part_pay,
-                                  mturk_fee,
-                                  tot_pay_mturk)),
-      Cost.MTurk.Masters = as.character(c(part_pay,
-                                          masters_fee,
-                                          tot_pay_masters)),
-      Cost.Prolific = as.character(c(part_pay,
-                                     prolific_fee,
-                                     tot_pay_prolific)),
+      Cost.MTurk = as.character(c(round(tot_pay, 2),
+                                  round(mturk_fee, 2),
+                                  round(tot_pay_mturk, 2))),
+      Cost.MTurk.Masters = as.character(c(round(tot_pay, 2),
+                                          round(masters_fee, 2),
+                                          round(tot_pay_masters, 2))),
+      Cost.CloudResearch = as.character(c(round(tot_pay, 2),
+                                          round(cr_fee, 2),
+                                          round(tot_pay_cr, 2))),
+      Cost.Prolific = as.character(c(round(tot_pay, 2),
+                                     round(prolific_fee, 2),
+                                     round(tot_pay_prolific, 2))),
       stringsAsFactors = FALSE)
     
   })
